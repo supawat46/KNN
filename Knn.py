d4bad0fb3872a05f858a69db01906baa34b45d28
supawat46 <yesnook9999@gmail.com>
2025-04-09 12:09:58 +0700

    .

--- Knn.py
+++ Knn.py
@@ -29,3 +29,9 @@ with col3:
 st.markdown(html_7, unsafe_allow_html=True)
 st.markdown("")
 st.markdown("")
+
+st.subheader("ข้อมูลส่วนแรก 10 แถว")
+dt = pd.read_csv("./data/iris-3.xlsx")
+st.write(dt.head(10))
+st.subheader("ข้อมูลส่วนสุดท้าย 10 แถว")
+st.write(dt.tail(10))
