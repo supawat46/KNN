e94b0d664fda3ab6174188727c19e0233b4a81b0
supawat46 <yesnook9999@gmail.com>
2025-04-09 13:50:02 +0700

    .

--- Knn.py
+++ Knn.py
@@ -2,6 +2,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('การจำแนกข้อมูลด้วยเทคนิค Machine Learning')
